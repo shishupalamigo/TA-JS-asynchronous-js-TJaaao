65088b1c89337aa60d9deb8338cb6efcbe7de103
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-10 00:31:13 +0530

    Add error msg and preLoader

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -2,34 +2,73 @@ let display = document.querySelector('.display');
 
 let source = document.querySelector('#source');
 
-let preLoader = document.createElement('img');
-preLoader.src = './assets/loader.gif';
-preLoader.setAttribute('class', 'display');
-display.append(preLoader);
+let main = document.querySelector(".main");
+let errorElm = document.querySelector(".error-message");
+
+function handleErrorMessage (message = "Something went wrong!") {
+  errorElm.style.display = "block";
+  main.style.display = "none";
+  errorElm.innerText = message;
+}
+
+function createLoader() {
+  let preLoader = document.createElement('div');
+  let preLoaderImg = document.createElement('img');
+  preLoaderImg.src = './assets/loader.gif';
+  preLoader.setAttribute('class', 'loader');
+  preLoader.append(preLoaderImg);
+  display.prepend(preLoader);
+}
+createLoader();
 
 function handleChange(event) {
   document.querySelector('.display').innerHTML = '';
+  createLoader();
   let data2 = fetch('https://api.spaceflightnewsapi.net/v3/articles?_limit=30')
     .then((res) => {
-      if (!res.ok) {
-        console.log(res);
-        throw new Error(`Error happened ${res.status}`);
+      if (res.ok) {
+      return res.json();
+      } else {
+        throw new Error("Respnse Not Ok!");
       }
-     return res.json()
     })
-    .then((userData) =>
+    .then((userData) => {
+      document.querySelector('.display').innerHTML = '';
       userData.forEach((data) => {
         if (data.newsSite == event.target.value) {
           createUI(data);
         }
-      })
-    ).catch(error => display.append(error));
+      });
+    })
+    .catch((error) => {
+      handleErrorMessage(error);
+    });
 }
 source.addEventListener('change', handleChange);
 
-let data = fetch('https://api.spaceflightnewsapi.net/v3/articles?_limit=30')
-  .then((res) => res.json())
-  .then((userData) => userData.forEach((data) => createUI(data)));
+function init() {
+  let data = fetch('https://api.spaceflightnewsapi.net/v3/articles?_limit=30')
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+        } else {
+          throw new Error("Respnse Not Ok!");
+        }
+    })
+    .then((userData) => {
+      document.querySelector('.display').innerHTML = '';
+
+      userData.forEach((data) => createUI(data));
+    })    .catch((error) => {
+      handleErrorMessage(error);
+    });
+}
+if (navigator.onLine) {
+  init();
+} else {
+handleErrorMessage("Check your internet connection! ❌");
+}
+
 
 function createUI(info) {
   let newsItem = document.createElement('div');
@@ -38,7 +77,7 @@ function createUI(info) {
   let article = document.createElement('article');
   let newsSite = document.createElement('span');
   let newsHeading = document.createElement('h2');
-  let newsSummary = document.createElement('p')
+  let newsSummary = document.createElement('p');
   let readMore = document.createElement('a');
 
   newsItem.setAttribute('class', 'article-container');
@@ -48,10 +87,10 @@ function createUI(info) {
   newsSummary.innerText = info.summary;
   newsSite.innerText = info.newsSite;
   readMore.href = info.url;
-  readMore.target = "_blank";
+  readMore.target = '_blank';
 
   newsImageContainer.append(newsImage);
-  article.append(newsSite, newsHeading, newsSummary,readMore);
+  article.append(newsSite, newsHeading, newsSummary, readMore);
   newsItem.append(newsImageContainer, article);
   display.append(newsItem);
 }
