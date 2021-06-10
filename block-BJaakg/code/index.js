2159ca7bc3b976508de784f5ae461d3d6ddc6f57
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-10 21:05:22 +0530

    Refactor code

--- block-BJaakg/code/index.js
+++ block-BJaakg/code/index.js
@@ -17,6 +17,7 @@
 
     const url = `https://www.anapioficeandfire.com/api/books`;
     
+    
     function displayCharacters(characters) {
         handleSpinner(charactersData, true);   
         Promise.all(characters.map((character) =>fetch(character).then((res) => res.json())))
@@ -24,12 +25,13 @@
             charactersData.innerHTML = "";
             data.forEach((character) => {
             let li = document.createElement("li");
-             li.innerText = `${character.name}: ${character.aliases.join(",  ")}`;
+
+             li.innerText = `${character.name} : (${character.aliases.join(",  ")})`;
+
             charactersData.append(li);
             
             })
-            
-            
+                       
         });
     
         modal.append(charactersData);
@@ -42,31 +44,38 @@
         books.forEach((book) => {
             let li = document.createElement("li");
             li.classList.add("book-container");
+
             let title = document.createElement("h2");
             title.innerText = book.name;
+
             let author = document.createElement("h3");
-            author.innerText = book.authors[0];
+            author.innerText = book.authors.join(' ');
+
             let numberOfPages = document.createElement("span");
             numberOfPages.innerText = `Number of Pages: ${book.numberOfPages}`;
+
             let publisher = document.createElement("span");
             publisher.innerText = `Publisher: ${book.publisher}`;
 
             let released = document.createElement("span");
             released.innerText = `Release Date: ${book.released.slice(0, 10)}`;
+
             let country = document.createElement("span");
             country.innerText = `Country: ${book.country}`;
 
             let button = document.createElement("a");
             button.innerText = `Show Characters ${book.characters.length}`;
+
             button.addEventListener("click", (e) => {
                 displayCharacters(book.characters);
                 modal.style.display = "block";
-            })
+            });
+
             li.append(title, author, numberOfPages, publisher,  released, country, button);
+
             ul.append(li);
         })
     }
-    
     
     
     function booksData() {
@@ -77,6 +86,5 @@
     }
             
     booksData();
-    // Promise.all(books.map((book) => book).then(console.log));
      
     })(); 
